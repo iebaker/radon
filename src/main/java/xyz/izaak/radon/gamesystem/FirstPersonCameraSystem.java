1b87027a4deadcb960ef8e34e8d3069ce229f24e
Izaak Baker <ibaker@palantir.com>
2016-12-21 20:15:17 -0500

    use scratch vector in first person camera

--- src/main/java/xyz/izaak/radon/gamesystem/FirstPersonCameraSystem.java
+++ src/main/java/xyz/izaak/radon/gamesystem/FirstPersonCameraSystem.java
@@ -1,6 +1,7 @@
 package xyz.izaak.radon.gamesystem;
 
 import org.joml.Vector2f;
+import org.joml.Vector3f;
 import xyz.izaak.radon.math.Points;
 import xyz.izaak.radon.world.Camera;
 
@@ -18,15 +19,17 @@ import static org.lwjgl.glfw.GLFW.GLFW_KEY_W;
  */
 public class FirstPersonCameraSystem implements GameSystem {
     private Camera camera;
+    private Vector3f scratch;
 
     public FirstPersonCameraSystem(Camera camera) {
         this.camera = camera;
+        this.scratch = new Vector3f();
     }
 
     @Override
     public void onMouseMove(Vector2f delta) {
         camera.rotate(delta.x / 150, Points.__z);
-        camera.rotate(delta.y / 150, Points.copyOf(camera.getUp()).cross(camera.getLook()));
+        camera.rotate(delta.y / 150, scratch.set(camera.getUp()).cross(camera.getLook()));
     }
 
     @Override
@@ -38,15 +41,15 @@ public class FirstPersonCameraSystem implements GameSystem {
                 break;
             case GLFW_KEY_DOWN:
             case GLFW_KEY_S:
-                camera.translate(Points.copyOf(camera.getLook()).negate());
+                camera.translate(scratch.set(camera.getLook()).negate());
                 break;
             case GLFW_KEY_LEFT:
             case GLFW_KEY_A:
-                camera.translate(Points.copyOf(camera.getUp()).cross(camera.getLook()));
+                camera.translate(scratch.set(camera.getUp()).cross(camera.getLook()));
                 break;
             case GLFW_KEY_RIGHT:
             case GLFW_KEY_D:
-                camera.translate(Points.copyOf(camera.getUp()).cross(camera.getLook()).negate());
+                camera.translate(scratch.set(camera.getUp()).cross(camera.getLook()).negate());
                 break;
         }
     }
