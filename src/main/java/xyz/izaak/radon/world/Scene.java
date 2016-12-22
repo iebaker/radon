bb1af4fae579473573b12e5422623ddeb7bdb4aa
Izaak Baker <ibaker@palantir.com>
2016-12-21 23:12:18 -0500

    Cut down on needless allocation

--- src/main/java/xyz/izaak/radon/world/Scene.java
+++ src/main/java/xyz/izaak/radon/world/Scene.java
@@ -7,9 +7,11 @@ import xyz.izaak.radon.shading.UniformProvider;
 import xyz.izaak.radon.shading.annotation.ProvidesShaderComponents;
 import xyz.izaak.radon.shading.annotation.ShaderUniform;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -30,6 +32,12 @@ public class Scene implements UniformProvider {
     private Set<Entity> entities = new HashSet<>();
     private Set<DirectionalLight> directionalLights = new HashSet<>();
     private Set<PointLight> pointLights = new HashSet<>();
+
+    private List<Vector3f> directionalLightDirections = new ArrayList<>(MAX_DIRECTIONAL_LIGHTS);
+    private List<Vector3f> directionalLightIntensities = new ArrayList<>(MAX_DIRECTIONAL_LIGHTS);
+    private List<Vector3f> pointLightPositions = new ArrayList<>(MAX_POINT_LIGHTS);
+    private List<Vector3f> pointLightIntensities = new ArrayList<>(MAX_POINT_LIGHTS);
+
     private Map<UUID, Portal> portalsById = new HashMap<>();
     private Vector3f gravity;
 
@@ -60,10 +68,14 @@ public class Scene implements UniformProvider {
 
     public void addPointLight(PointLight pointLight) {
         pointLights.add(pointLight);
+        pointLightPositions.add(pointLight.getPosition());
+        pointLightIntensities.add(pointLight.getIntensity());
     }
 
     public void addDirectionalLight(DirectionalLight directionalLight) {
         directionalLights.add(directionalLight);
+        directionalLightDirections.add(directionalLight.getDirection());
+        directionalLightIntensities.add(directionalLight.getIntensity());
     }
 
     public void addPortal(Portal portal) {
@@ -103,31 +115,43 @@ public class Scene implements UniformProvider {
 
     @ShaderUniform(length = MAX_DIRECTIONAL_LIGHTS, identifier = Identifiers.DIRECTIONAL_LIGHT_DIRECTIONS)
     public List<Vector3f> getDirectionalLightDirections() {
-        return directionalLights.stream().map(DirectionalLight::getDirection).collect(Collectors.toList());
+        return directionalLightDirections;
     }
 
     @ShaderUniform(length = MAX_DIRECTIONAL_LIGHTS, identifier = Identifiers.DIRECTIONAL_LIGHT_INTENSITIES)
     public List<Vector3f> getDirectionalLightIntensities() {
-        return directionalLights.stream().map(DirectionalLight::getIntensity).collect(Collectors.toList());
+        return directionalLightIntensities;
     }
 
     @ShaderUniform(length = MAX_POINT_LIGHTS, identifier = Identifiers.POINT_LIGHT_POSITIONS)
     public List<Vector3f> getPointLightPositions() {
-        return pointLights.stream().map(PointLight::getPosition).collect(Collectors.toList());
+        return pointLightPositions;
     }
 
     @ShaderUniform(length = MAX_POINT_LIGHTS, identifier = Identifiers.POINT_LIGHT_INTENSITIES)
     public List<Vector3f> getPointLightIntensities() {
-        return pointLights.stream().map(PointLight::getIntensity).collect(Collectors.toList());
+        return pointLightIntensities;
     }
 
     @Override
     public void setUniformsOn(Shader shader) {
         shader.setUniform(Identifiers.DIRECTIONAL_LIGHT_COUNT, getNumDirectionalLights());
         shader.setUniform(Identifiers.POINT_LIGHT_COUNT, getNumPointLights());
-        shader.setUniform(Identifiers.DIRECTIONAL_LIGHT_DIRECTIONS, getDirectionalLightDirections());
-        shader.setUniform(Identifiers.DIRECTIONAL_LIGHT_INTENSITIES, getDirectionalLightIntensities());
-        shader.setUniform(Identifiers.POINT_LIGHT_POSITIONS, getPointLightPositions());
-        shader.setUniform(Identifiers.POINT_LIGHT_INTENSITIES, getPointLightIntensities());
+        shader.setUniform(
+                Identifiers.DIRECTIONAL_LIGHT_DIRECTIONS,
+                MAX_DIRECTIONAL_LIGHTS,
+                getDirectionalLightDirections());
+        shader.setUniform(
+                Identifiers.DIRECTIONAL_LIGHT_INTENSITIES,
+                MAX_DIRECTIONAL_LIGHTS,
+                getDirectionalLightIntensities());
+        shader.setUniform(
+                Identifiers.POINT_LIGHT_POSITIONS,
+                MAX_POINT_LIGHTS,
+                getPointLightPositions());
+        shader.setUniform(
+                Identifiers.POINT_LIGHT_INTENSITIES,
+                MAX_POINT_LIGHTS,
+                getPointLightIntensities());
     }
 }
